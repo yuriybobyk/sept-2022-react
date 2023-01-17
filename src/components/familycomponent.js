ad1e21b3bc9472233021d7e4c09b8e4fdb92039a
yuriybobyk <yuriybobyk4325@gmail.com>
2023-01-17 16:37:24 +0200

    HW1 done

--- src/components/familycomponent.js
+++ src/components/familycomponent.js
@@ -1,11 +1,12 @@
 import React from "react";
+import './familycomponent.css'
 export const FamilyComponent = ({familyitem, familypic, familydesc}) =>{
     const classfamilyname = 'family';
     return (
         <div className={classfamilyname}>
-            <h2>{familyitem}</h2>
-            <img src={familypic} alt={'family'}/>
-            <p>{familydesc}</p>
+            <h2 className={'title'}>{familyitem}</h2>
+            <img className={'familypic'} src={familypic} alt={'family'}/>
+            <p className={'desc'}>{familydesc}</p>
         </div>
     )
 }
