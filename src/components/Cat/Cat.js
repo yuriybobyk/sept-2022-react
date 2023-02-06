52c664c3ad92bedf08a3c0b04ff8f77f5f6c449b
yuriybobyk <yuriybobyk4325@gmail.com>
2023-02-06 12:19:52 +0200

    Start hw5 part 2

---
+++ src/components/Cat/Cat.js
@@ -0,0 +1,9 @@
+const Cat = () => {
+    return (
+        <div>
+            Cat
+        </div>
+    );
+};
+
+export {Cat};
---
+++ src/components/Dog/Dog.js
@@ -0,0 +1,9 @@
+const Dog = () => {
+    return (
+        <div>
+            Dog
+        </div>
+    );
+};
+
+export {Dog};
