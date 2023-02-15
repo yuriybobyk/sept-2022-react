63f09fe2df45bea97f7b538a171f48412f8062f8
yuriybobyk <yuriybobyk4325@gmail.com>
2023-02-15 16:05:59 +0200

    hw8 done

---
+++ app.ts
@@ -0,0 +1,65 @@
+interface Icores {
+
+    flight: number,
+    core: {
+        reuse_count: number,
+        status: string
+    }
+
+}
+
+interface Ipayloads {
+    payload_type: string,
+    payload_mass_kg: number,
+    payload_mass_lbs: number
+}
+
+interface ISpaceX {
+    mission_name: string,
+    launch_date_local: string,
+    launch_site: {
+        site_name_long: string
+    },
+    links: {
+        article_link: string,
+        video_link: string
+    },
+    rocket: {
+        rocket_name: string,
+        first_stage: {
+            cores: Icores[]
+        },
+        second_stage: {
+            payloads: Ipayloads[]
+        }
+    }
+}
+
+// ---------------------------------------------------------------------------—————————---------------------------------
+
+interface IUser {
+    name: string;
+    age: number;
+    gender: string;
+}
+const user:IUser = {
+    name:"Max",
+    age:18,
+    gender:'male'
+}
+
+function sum(a:number,b:number):number{
+    return a+b
+}
+function showSum(a:number,b:number):void{
+    console.log(a + b);
+}
+
+function incAge(someUser:IUser, inc:number):IUser {
+    someUser.age+=inc
+    return someUser
+}
+
+console.log(sum(1, 2));
+showSum(2,3)
+incAge(user, 2)
